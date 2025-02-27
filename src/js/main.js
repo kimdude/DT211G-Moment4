1980fed7f57a97be552df74d5378ae7f7eceb1cd
Kim Dudenhöfer <kidu2400@student.miun.se>
2025-02-27 14:37:13 +0100

    Lagt till laddningsikon vid klick på knapp under animation-sidan.

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,13 @@
 const openBtn = document.getElementById("openMenu");
 const closeBtn = document.getElementById("closeMenu");
 const mainMenu = document.getElementById("navContainer");
+const loadBtn = document.getElementById("btn");
+const loadIcon = document.getElementById("loadIcon");
 
 //Checking value of null and adding eventlisteners if variable exists
 openBtn.addEventListener('click', displayMenu);
 closeBtn.addEventListener('click', displayMenu);
+loadBtn.addEventListener('click', displayLoad);
 
 //Toggle main menu i mobile-version
 function displayMenu() {
@@ -18,5 +21,16 @@ function displayMenu() {
     } else {
         mainMenu.style.display = "none";
         openBtn.style.display = "block";
+    }
+}
+
+//Toggle load Icon
+function displayLoad() {
+    const style = window.getComputedStyle(loadIcon);
+
+    if(style.visibility === "hidden") {
+        loadIcon.style.visibility = "visible";
+    } else {
+        loadIcon.style.visibility = "hidden";
     }
 }
